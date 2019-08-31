ea20967d8ff7e6f53b2bc1617ef8559b47c630d9
Morten Eek <mortenee@gmail.com>
2019-08-31 19:30:44 +0200

    first commit

---
+++ init.lua
@@ -0,0 +1,13 @@
+-- ui
+-- events.connect(events.INITIALIZED, function() textadept.menu.menubar = nil end)
+-- ui.tabs = false
+ui.size = {1100, 900} -- width, height
+
+-- general options
+textadept.editing.strip_trailing_spaces = true
+buffer.tab_width = 4
+buffer.use_tabs = false
+buffer.tab_indents = true
+buffer.back_space_un_indents = true
+
+
